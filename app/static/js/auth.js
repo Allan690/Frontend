1eeecc6c5658db573673964df9ebeca03ab9e821
charisschomba <charisschomba@gmail.com>
2018-08-09 12:47:29 +0300

    combined signin and signup scripts into a single file

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,78 +1,76 @@
 // Register new user
 
 const signUp = () => {
-    document.getElementById("signup").addEventListener("submit", function (e) {
-  e.preventDefault();
-  data = {
+  document.getElementById("signup").addEventListener("submit", function(e) {
+    e.preventDefault();
+    data = {
       username: document.getElementById("username").value,
       email: document.getElementById("email").value,
       password: document.getElementById("password").value,
       confirm_password: document.getElementById("confirmPassword").value,
-  };
+    };
 
-  fetch("https://diaryapi-v2.herokuapp.com/mydiary/v1/auth/register", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(data)
+    fetch("https://diaryapi-v2.herokuapp.com/mydiary/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
       })
       .then(res => res.json())
 
       .then(data => {
-          if (data.message === "Your account was created") {
-             let msg = data.message;
-             document.getElementById("white").innerHTML = msg;
-              window.location.href = "/signin";
-          }
-          else {
-              let msg = Object.values(data);
-              console.log(msg)
-              document.getElementById("white").innerHTML = msg;
-          }
+        if (data.message === "Your account was created") {
+          let msg = data.message;
+          document.getElementById("white").innerHTML = msg;
+          window.location.href = "/signin";
+        } else {
+          let msg = Object.values(data);
+          console.log(msg)
+          document.getElementById("white").innerHTML = msg;
+        }
 
       })
       .catch(error => console.error("Error:", error));
-});
+  });
 
 }
 // Login existing users
 
 const signIn = () => {
- document.getElementById('signin').addEventListener('submit',function(e){
-e.preventDefault();
+  document.getElementById('signin').addEventListener('submit', function(e) {
+    e.preventDefault();
 
-data = {
-        email: document.getElementById("userEmail").value,
-        password: document.getElementById("userPassword").value
-        };
+    data = {
+      email: document.getElementById("userEmail").value,
+      password: document.getElementById("userPassword").value
+    };
 
-fetch("https://diaryapi-v2.herokuapp.com/mydiary/v1/auth/login", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-})
-.then(res => res.json())
-.then(data =>{
+    fetch("https://diaryapi-v2.herokuapp.com/mydiary/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .then(data => {
         let res = Object.values(data);
         const access_token = res[0];
-        if(data.token === access_token){
-            msg = "Login was successful";
-            document.getElementById('white').innerHTML = msg;
-            localStorage.setItem('token',access_token);
-            window.location.href = "/home";
+        if (data.token === access_token) {
+          msg = "Login was successful";
+          document.getElementById('white').innerHTML = msg;
+          localStorage.setItem('token', access_token);
+          window.location.href = "/home";
+        } else {
+          let msg = Object.values(data);
+          console.log(msg)
+          document.getElementById('white').innerHTML = msg;
         }
 
-        else{
-            let msg  = Object.values(data);
-            console.log(msg)
-            document.getElementById('white').innerHTML = msg;
-        }
+      })
+    .catch(error => console.error("Error:", error));
 
-    });
-});
+  });
+
 }
-
-
