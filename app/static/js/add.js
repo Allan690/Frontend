1e1ff5353f1a69ca7d357c58165888770cc11915
charisschomba <charisschomba@gmail.com>
2018-08-09 12:40:16 +0300

    [ft-#159461129] implemented add entry feature

---
+++ app/static/js/add.js
@@ -0,0 +1,48 @@
+const add = () => {
+document.getElementById("entry").addEventListener("submit", function (e) {
+    e.preventDefault();
+    data = {
+        title: document.getElementById("title").value,
+        content: document.getElementById("content").value
+    };
+
+token = localStorage.getItem("token");
+fetch('https://diaryapi-v2.herokuapp.com/mydiary/v1/entries',{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json",
+    Authorization: `Bearer ${token}`
+  },
+  body: JSON.stringify(data)
+
+})
+        .then(res => res.json())
+
+        .then(data => {
+            console.log(data.message)
+            if(data.message === "successfully added"){
+                let msg = data.message;
+                document.getElementById("white").innerHTML = msg;
+                window.location.href = "/home";
+            }
+            else{
+                let msg = Object.values(data);
+                document.getElementById("white").innerHTML = msg;
+            }
+        })
+
+        .catch(error => console.error("Error:", error));
+
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
